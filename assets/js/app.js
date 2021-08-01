6ddd39db65cf991f61e449bfce1a902dde2b3ff0
f-rahim <freelancerrahim26@gmail.com>
2021-08-02 01:59:40 +0600

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $("#happyUser").owlCarousel({
+    items: 2,
+    loop: true,
+    nav: true,
+    navText: [
+      "<img src='assets/images/arrow/arrow-left.png'>",
+      "<img src='assets/images/arrow/arrow-right.png'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
+
+  // client logo
+  $("#clientSlide").owlCarousel({
+    items: 6,
+    loop: true,
+    nav: true,
+    navText: [
+      "<img src='assets/images/arrow/c-arrow-left.png'>",
+      "<img src='assets/images/arrow/c-arrow-right.png'>",
+    ],
+    autoPlay: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      480: {
+        items: 3,
+      },
+      768: {
+        items: 4,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
+});
