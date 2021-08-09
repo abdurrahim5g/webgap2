4f168c27eb66376df5d13f1675e20da51aa165c4
f-rahim <freelancerrahim26@gmail.com>
2021-08-10 03:33:56 +0600

    fix parallax issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  // header-sticky
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 0) {
+      $("#sticky").addClass("is-sticky");
+    } else {
+      $("#sticky").removeClass("is-sticky");
+    }
+  });
+
   // slick nav
   $("#menu").slicknav();
 
@@ -8,6 +18,8 @@ $(document).ready(function () {
     loop: true,
     nav: true,
     autoplay: true,
+    smartSpeed: 500,
+
     navText: [
       "<img src='assets/images/svg_icons/slider_arrow_white.svg'>",
       "<img src='assets/images/svg_icons/slider_arrow_white.svg'>",
@@ -24,27 +36,42 @@ $(document).ready(function () {
 
   // client logo
   $("#clientSlide").owlCarousel({
-    items: 6,
+    items: 5,
     loop: true,
     nav: true,
-    // autoplay: true,
+    center: true,
+    autoplay: true,
+    smartSpeed: 500,
+
     navText: [
       "<img src='assets/images/svg_icons/slider_arrow_blue.svg'>",
       "<img src='assets/images/svg_icons/slider_arrow_blue.svg'>",
     ],
     responsive: {
       0: {
-        items: 2,
+        items: 1,
       },
       480: {
-        items: 3,
+        items: 1,
       },
       768: {
-        items: 4,
+        items: 3,
       },
       1000: {
-        items: 6,
+        items: 5,
       },
     },
+  });
+
+  // parallax
+  $("#happyUserParallax").parallaxie({
+    speed: 0.5,
+    offset: 0,
+    repeat: "repeat-y",
+  });
+  $("#signUpParallax").parallaxie({
+    speed: 0.5,
+    offset: 0,
+    repeat: "repeat-y",
   });
 });
